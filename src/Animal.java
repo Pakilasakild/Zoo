f904d78c0fa1a9e29678e2b00352fd38a3a6e581
pakilasakild <dnasknow@gmail.com>
2025-11-20 10:33:36 +0200

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,39 @@
+abstract public class Animal {
+    private String name;
+    private int age;
+    private String species;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public Animal(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+    public abstract void makeSound();
+    public String getInfo(){
+        return name + ", age is: " + age + ", " + species;
+    }
+}
